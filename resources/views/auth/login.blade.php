5e88d23e069ddd25781424658c541cfbb7423745
sandra <sandra.h.arriaga@gmail.com>
2020-05-28 01:47:57 -0500

    add new design to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,57 @@
-@extends('layouts.app')
+@extends('layouts.login_app')
 
 @section('content')
+<!--<h2>Acceso</h2>-->
+<form action="{{ route('login') }}" method="POST">
+    @csrf
+    <div class=" w3l-form-group">
+        <label>Usuario:</label>
+        <div class="group">
+            <i class="fas fa-user"></i>
+            <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Username" required="required" />
+
+        </div>
+    </div>
+
+    @error('email')
+
+    <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
+    </span>
+
+    @enderror
+
+    <div class=" w3l-form-group">
+        <label>Contraseña:</label>
+        <div class="group">
+            <i class="fas fa-unlock"></i>
+            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required="required" />
+        </div>
+    </div>
+
+    @error('password')
+    <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
+    </span>
+    @enderror
+
+
+    <div class="forgot">
+        @if (Route::has('password.request'))
+        <a href="{{ route('password.request') }}">
+            {{ __('Forgot Your Password?') }}
+        </a>
+        @endif
+
+        <p><input type="checkbox">Recordar su datos</p>
+    </div>
+    <button type="submit">Login</button>
+</form>
+<p class=" w3l-register-p">¿No tienes cuenta?<a href="{{ route('register') }}" class="register"> Registrar</a></p>
+
+
+
+<!--
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -70,4 +121,6 @@
         </div>
     </div>
 </div>
+
+-->
 @endsection
---
+++ resources/views/layouts/login_app.blade.php
@@ -0,0 +1,80 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <title>WAPOS - Login</title>
+     <!-- CSRF Token -->
+     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- meta tags -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="keywords" content="WAPOS"
+    />
+    <!-- /meta tags -->
+
+
+    <script src="{{ asset('js/app.js') }}" ></script>
+    <script src="https://kit.fontawesome.com/9a331721e8.js" crossorigin="anonymous"></script>
+
+
+
+    <!-- custom style sheet -->
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet" type="text/css" >
+
+    <!-- /custom style sheet -->
+    <!-- fontawesome css -->
+    <link href="css/fontawesome-all.css" rel="stylesheet" />
+    <!-- /fontawesome css -->
+    <!-- google fonts-->
+    <link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <!-- /google fonts-->
+</head>
+
+<body>
+
+    <h1></h1>
+    <div class=" w3l-login-form">
+        <a href="{{ url('/') }}"><span id="logo"><img src="{{ URL::asset('images/WAPOS.png')}}" alt="logo" class="video img-responsive animation-box wow animated bounceInDown" style="width: 400px;"></a>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+        </div>
+    <footer>
+        <p class="copyright-agileinfo"> &copy; 2020 WAPOS. All Rights Reserved | BDG </p>
+    </footer>
+
+    <script>
+            function checkPhoneNumber(e) {
+                tecla = (document.all) ? e.keyCode : e.which;
+
+                //Tecla de retroceso para borrar, siempre la permite
+                if (tecla == 8) {
+                    return true;
+                }
+
+                var value = document.getElementById('phone-number-client').value;
+                if (value.length > 9) {
+                    return false; // keep form from submitting
+                }
+                // Patron de entrada, en este caso solo acepta numeros y letras
+                patron = /[0-9]/;
+                tecla_final = String.fromCharCode(tecla);
+                return patron.test(tecla_final);
+            }
+        </script>
+
+</body>
+
+</html>
+
+
+        
