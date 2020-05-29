03f076139eb7f50a036ee5107b4bc797f0c6e7f0
sandra <sandra.h.arriaga@gmail.com>
2020-05-28 23:22:14 -0500

    update views

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.login_app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -62,4 +63,5 @@
         </div>
     </div>
 </div>
+
 @endsection
